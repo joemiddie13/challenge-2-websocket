2bc9273a9db3cc917cf2df695fda9bf9fdb2d0d1
joemiddie13 <joemiddie13@gmail.com>
2024-06-21 19:50:38 -0700

    added username or nicknames

--- index.js
+++ index.js
@@ -11,17 +11,29 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  io.emit('chat message', 'A user has connected');
+    console.log('a user connected');
 
-  socket.on('disconnect', () => {
-      console.log('user disconnected');
-      io.emit('chat message', 'A user has disconnected');
-  });
+    socket.on('set nickname', (nickname) => {
+        socket.nickname = nickname;
+        io.emit('chat message', `${nickname} has joined the chat`);
+        console.log(`${nickname} has joined the chat`);
+    });
 
-  socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
-  });
+    socket.on('disconnect', () => {
+        if (socket.nickname) {
+            io.emit('chat message', `${socket.nickname} has left the chat`);
+            console.log(`${socket.nickname} has left the chat`);
+        } else {
+            io.emit('chat message', 'A user has disconnected');
+            console.log('A user has disconnected');
+        }
+    });
+
+    socket.on('chat message', (msg) => {
+        const message = `${socket.nickname || 'Anonymous'}: ${msg}`;
+        io.emit('chat message', message);
+        console.log(message);
+    });
 });
 
 server.listen(3000, () => {
