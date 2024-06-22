3c59eb8ecb730fae62c15864a11a452192582825
joemiddie13 <joemiddie13@gmail.com>
2024-06-21 19:56:22 -0700

    display the username connected and writing messages

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+let users = {};
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
@@ -15,6 +17,8 @@ io.on('connection', (socket) => {
 
     socket.on('set nickname', (nickname) => {
         socket.nickname = nickname;
+        users[socket.id] = nickname;
+        io.emit('user list', Object.values(users));
         io.emit('chat message', `${nickname} has joined the chat`);
         console.log(`${nickname} has joined the chat`);
     });
@@ -23,6 +27,8 @@ io.on('connection', (socket) => {
         if (socket.nickname) {
             io.emit('chat message', `${socket.nickname} has left the chat`);
             console.log(`${socket.nickname} has left the chat`);
+            delete users[socket.id];
+            io.emit('user list', Object.values(users));
         } else {
             io.emit('chat message', 'A user has disconnected');
             console.log('A user has disconnected');
@@ -33,6 +39,10 @@ io.on('connection', (socket) => {
         const message = `${socket.nickname || 'Anonymous'}: ${msg}`;
         io.emit('chat message', message);
         console.log(message);
+    });
+
+    socket.on('typing', (isTyping) => {
+        socket.broadcast.emit('typing', { user: socket.nickname, isTyping });
     });
 });
 
